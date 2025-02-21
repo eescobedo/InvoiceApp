63997c32cf12dfbfd1d3efed361c652c68169367
Enner <enner.escobedo@gmail.com>
2025-02-21 15:54:53 -0600

    Initial commit

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceApp.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
