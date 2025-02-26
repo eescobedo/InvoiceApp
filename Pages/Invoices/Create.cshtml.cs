6172bf52240de1a0e15c1d91f62c03c1554adca1
Enner <eriec.42@gmail.com>
2025-02-26 16:21:32 -0600

    Add section to create invoice form

--- Pages/Invoices/Create.cshtml.cs
+++ Pages/Invoices/Create.cshtml.cs
@@ -1,9 +1,15 @@
+using InvoiceApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InvoiceApp.Pages.Invoices
 {
   public class CreateModel : PageModel
   {
+
+    [BindProperty]
+    public InvoiceDto InvoiceDto { get; set; } = new InvoiceDto();
+
     public void OnGet()
     {
     }
