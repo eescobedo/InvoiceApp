2288ce30b77907283ee8bcdbde143535e9eb5247
Enner <eriec.42@gmail.com>
2025-02-27 15:49:09 -0600

    Add edit and delete section to the invoices section

---
+++ Pages/Invoices/Delete.cshtml.cs
@@ -0,0 +1,28 @@
+using InvoiceApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InvoiceApp.Pages.Invoices;
+
+public class DeleteModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public DeleteModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public IActionResult OnGet(int id)
+    {
+        var invoice = _context.Invoices.Find(id);
+
+        if (invoice != null)
+        {
+            _context.Invoices.Remove(invoice);
+            _context.SaveChanges();
+        }
+        
+        return RedirectToPage("/Invoices/Index");
+    }
+}
