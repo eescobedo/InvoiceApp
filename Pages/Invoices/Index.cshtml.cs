83a6f2e1d31abacb6601cdc0530d3246785d308a
Enner <eriec.42@gmail.com>
2025-02-28 09:14:07 -0600

    Add simple pagination to list of invoices

--- Pages/Invoices/Index.cshtml.cs
+++ Pages/Invoices/Index.cshtml.cs
@@ -1,6 +1,8 @@
 using InvoiceApp.Models;
 using InvoiceApp.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace InvoiceApp.Pages.Invoices
 {
@@ -9,15 +11,34 @@ namespace InvoiceApp.Pages.Invoices
     private readonly ApplicationDbContext context;
     public List<Invoice> invoices = new();
 
+    public int CurrentPage { get; set; } = 1;
+    public int TotalPages { get; set; }
+    public const int ItemsPerPage = 10;
+
+
     public IndexModel(ApplicationDbContext context)
     {
       this.context = context;
     }
 
-
-    public void OnGet()
+    public void OnGet(int currentPage = 1)
     {
-      invoices = context.Invoices.OrderByDescending(i => i.Id).ToList();
+      CurrentPage = currentPage;
+      var totalItems = context.Invoices.Count();
+      TotalPages = totalItems / ItemsPerPage;
+      if (totalItems % ItemsPerPage > 0)
+      {
+        TotalPages++;
+      }
+
+      invoices = context.Invoices
+        .OrderByDescending(i => i.Id)
+        .Skip((CurrentPage - 1) * ItemsPerPage)
+        .Take(ItemsPerPage)
+        .ToList();
     }
+    /*    {
+          invoices = context.Invoices.OrderByDescending(i => i.Id).ToList();
+      }*/
   }
 }
